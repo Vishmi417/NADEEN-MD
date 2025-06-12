324a51c4f798c406456deba778cc67259bac004b
Vishmi417 <rathnayakavishmitha5@gmail.com>
2025-06-12 16:45:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=CYNVVCSB#yA5QGLytAQBl4SnxooY9Sp7VjGiPzC_VUd-8_yMsr7Q' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=PEMDSJ6T#Thbnhxe1kkDHY3FcxP_qH1rODRQUC6czjubXYVR2N1w' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
